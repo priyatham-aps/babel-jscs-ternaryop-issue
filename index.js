f236d374613343ee3c67bac144126d44290c3269
Priyatham Adigopula <priyatham@modelogiq.com>
2015-07-21 19:29:19 +0530

    Added few more scenarios

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+//Creates issue
 let toogleStatus = node.removed ?
 	(self.state.addNode !== '' ? !(self.state.addNode.split('_')[0] === node.id) : true)
 	: (self.state.removeNode !== '' ? (self.state.removeNode.split('_')[0] === node.id) : false);
+
+//Creates issue
+toogleStatus = self.state.removeNode !== '' ? (self.state.removeNode.split('_')[0] === node.id) : false
+
+//Does not create issue
+toogleStatus = self.state.removeNode !== '' ? self.state.removeNode.split('_')[0] === node.id : false
