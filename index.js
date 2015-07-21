7bc84f438cdd0f09814e53b8cde22bd498ab4cb1
Priyatham Adigopula <priyatham@modelogiq.com>
2015-07-21 13:59:44 +0530

    Minimal repo to demonstrate babel-jscs issue

---
+++ index.js
@@ -0,0 +1,3 @@
+let toogleStatus = node.removed ?
+	(self.state.addNode !== '' ? !(self.state.addNode.split('_')[0] === node.id) : true)
+	: (self.state.removeNode !== '' ? (self.state.removeNode.split('_')[0] === node.id) : false);
